f28843eb2538eb343e66cbce5d6be83b7d354276
Ravinder Sollet <solletravinder@gmail.com>
2022-03-27 16:19:35 +0530

    added main condition to make it standalone app

--- main.py
+++ main.py
@@ -134,6 +134,7 @@ class Application(tk.Frame):
     # def __remain_num(self, a, b):
     #     return a % b if a and b else 0
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
